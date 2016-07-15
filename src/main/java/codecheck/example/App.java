f2f23d22beeac3e5f0fc38d1c79df64f7d0df2dd
sperku <sperku@gmail.com>
2016-07-15 12:09:34 +0900

    Update App.java

--- src/main/java/codecheck/example/App.java
+++ src/main/java/codecheck/example/App.java
@@ -18,33 +18,34 @@ public class App {
       int end = 1;
     	while (start <= param) {
           	start++;
-        	String s = String.valueOf(end);
+        	  String s = String.valueOf(end);
           	int i = 0;
-        	while (i <= (s.length()-1)) {
+        	  while (i <= (s.length()-1)) {
             	String str1 = s.substring(i, i + 1);
-               	String str2 = s.substring(i + 1, i + 2);
+              String str2 = s.substring(i + 1, i + 2);
               		if (str1 === str2 ){
-                    	if (i = 0)
+                    	if (i == 0) {
                     		String pend;
                     		pend = (str1 + str2);
-                    	}
-              			else {
-                        	pend += str2;
-                        }
+                      }
+              		    else {
+                        pend += str2;
+                      }
+              		}
               		else {
                       String ans;
                       	if (i = 0) {
                         int n = str1.length();
                        
                         }
-              			else {
+              			    else {
                         int n = pend.length();
                         } 
                       String j = String.valueOf(n);
                       String k = str1;
                       ans = j + k;
                       result += ans;
-                    }
+                  }
               i++;
             }
         }
